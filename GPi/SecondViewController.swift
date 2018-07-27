558281d0f1636f8570f76cf5b5642d9be377eb9f
Mukul Keshar <muk546@me.com>
2018-07-26 23:55:50 -0400

    Initial Commit

---
+++ GPi/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GPi
+//
+//  Created by Mukul on 7/26/18.
+//  Copyright © 2018 Mukul. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
